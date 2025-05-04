996c5f27215b2ef91486c95ebabee47c09f780fd
Idba1 <moniraislamself@gmail.com>
2025-05-04 17:36:04 +0600

    sidebar

--- client/src/Components/Sidebar.jsx
+++ client/src/Components/Sidebar.jsx
@@ -1,8 +1,9 @@
-import { FaTshirt, FaShoePrints, FaClock, FaSnowflake, FaGlasses, FaTabletAlt } from "react-icons/fa";
+import { FaHome, FaTshirt, FaShoePrints, FaClock, FaSnowflake, FaGlasses, FaTabletAlt } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
     const menuItems = [
+        { name: "Home", icon: <FaHome />, path: "/" },
         { name: "Clothes", icon: <FaTshirt />, path: "/category/clothes" },
         { name: "Shoes", icon: <FaShoePrints />, path: "/category/shoes" },
         { name: "Watches", icon: <FaClock />, path: "/category/watches" },
@@ -11,19 +12,15 @@ const Sidebar = () => {
         { name: "Electronics", icon: <FaTabletAlt />, path: "/category/electronics" },
     ];
 
-
     return (
         <div className="h-screen flex flex-col bg-white border-r p-4">
-            {/* <h2 className="text-xl font-bold mb-6 text-center">E-24 Menu</h2> */}
-
             <nav className="flex flex-col space-y-4 flex-grow">
                 {menuItems.map((item) => (
                     <NavLink
                         key={item.name}
                         to={item.path}
                         className={({ isActive }) =>
-                            `flex items-center space-x-3 p-2 rounded hover:bg-gray-100 transition ${isActive ? "bg-gray-200 font-semibold" : ""
-                            }`
+                            `flex items-center space-x-3 p-2 rounded hover:bg-gray-100 transition ${isActive ? "bg-gray-200 font-semibold" : ""}`
                         }
                     >
                         <span className="text-lg">{item.icon}</span>
